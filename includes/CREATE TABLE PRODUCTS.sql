8c5bf91270410b658a38c2884c1217e9ea9d184b
FOOL <elcarlwenguirhem@gmail.com>
2023-06-09 12:28:34 +0800

    hh

--- includes/CREATE TABLE PRODUCTS.sql
+++ includes/CREATE TABLE PRODUCTS.sql
@@ -9,11 +9,11 @@ reviews INT NOT NULL,
 stars INT NOT NULL,
 stocks INT NOT NULL);
 
-LOAD DATA INFILE 'C:/Users/63948/Downloads/Mugs.csv'
+LOAD DATA INFILE 'C:/Users/63948/Downloads/products.csv'
 INTO TABLE products
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
+LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
 LOAD DATA INFILE 'C:/Users/63948/Downloads/Mousepads.csv'
@@ -23,7 +23,7 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
- alter table products
+alter table products
 add column status varchar(250) as (
 case
 when stocks = 0 then 'Out of Stock'
@@ -35,12 +35,19 @@ select cart_id, item_id, name, theme, quantity, price, subtotal, (select sum(sub
 from displayItems;
 
 create view order_summary as
-select C.cart_id, I.item_id, P.name, T.theme, I.quantity, P.price, P.price * I.quantity as subtotal
-from Cart C, cart_items I, products P, themes T where C.cart_id = I.cart_id and P.id = I.product_id and P.theme_id = T.id;
+    select C.cart_id, I.item_id, Im.image, P.name, T.theme, I.quantity, P.price, I.design, P.price * I.quantity as subtotal
+    from Cart C, cart_items I, products P, themes T, images Im where P.id = Im.product_id AND C.cart_id = I.cart_id and P.id = I.product_id and P.theme_id = T.id and Im.type = "primary" ;
 
 create view productDetails as
 select P.id, P.name, T.theme, C.category, P.price, P.status, D.details, P.reviews, P.stars
 from products P, themes T, categories C, details D where P.category_id = C.id and P.theme_id = T.id and P.details_id = D.id;
+
+alter table cart_items 
+add column img int as (
+case
+when design = 0 then 1
+else design
+end);
 
 LOAD DATA INFILE 'C:/Users/63948/Downloads/Images.csv'
 INTO TABLE images
@@ -53,7 +60,7 @@ create table images
 (id int not null auto_increment primary key,
 product_id int not null,
 image varchar(250) not null,
-status varchar(250));
+type varchar(250));
 
 alter table images
 add foreign key (product_id) references products(id);
@@ -61,3 +68,6 @@ add foreign key (product_id) references products(id);
 create view wishlistDisplay as
 select WI.id, U.user_id, I.product_id, WI.wishlist_id, I.image, P.name, P.status, P.price
 from wishlist W, wishlist_items WI, images I, products P, users U where W.id = wishlist_id AND P.id = WI.product_id AND U.user_id = W.user_id AND I.product_id = P.id AND I.type = 'primary';
+
+create view orders as 
+select C.user_id, Im.image, P.name, 
